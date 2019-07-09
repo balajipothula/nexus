e9206ad3e182638012eeafb03853c3abdc267802
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-09 17:47:20 +0530

    Update setup-nexus.sh

--- setup-nexus.sh
+++ setup-nexus.sh
@@ -4,6 +4,8 @@
 # Date        : 31 August 2016,
 # Description : Installing nexus on rhel, ubuntu.
 
+# https://help.sonatype.com/repomanager2/download#Download-NexusRepositoryManager2OSS
+
 # installing git java-1.8.0 on ubuntu.
 # sudo apt -y install git openjdk-8-jdk
 
@@ -11,13 +13,13 @@
 sudo yum -y install git java-1.8.0-openjdk
 
 # downloading nexus
-wget https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz
+curl -J -L https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz -o nexus.tar.gz
 
 # extracting nexus tarball.
-tar xzf nexus-latest-bundle.tar.gz
+tar xzf nexus.tar.gz
 
 # renaming nexus-VERSION to nexus-server.
-mv nexus-* nexus-server
+mv nexus-* nexus
 
 # starting nexus server.
-sh $HOME/nexus-server/bin/nexus start
+sh $HOME/nexus/bin/nexus start
