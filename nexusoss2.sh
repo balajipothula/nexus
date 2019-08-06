0b04f8cac59a954f33ad4db9630b0d8815b63589
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-06 10:41:08 +0530

    Update nexusoss2.sh

--- nexusoss2.sh
+++ nexusoss2.sh
@@ -5,7 +5,7 @@ sudo yum -y install java-1.8.0-openjdk
 curl -J -L https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz -o $HOME/nexus.tar.gz && \
 tar xzf $HOME/nexus.tar.gz -C $HOME/                                                                && \
 rm -rf  $HOME/nexus.tar.gz                                                                          && \
-mv      $HOME/nexus-* $HOME/.nexusoss2                                                              && \
+mv      $HOME/nexus-*       $HOME/.nexusoss2                                                        && \
 mv      $HOME/sonatype-work $HOME/.artifactory                                                      && \
 find    $HOME/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                              && \
 find    $HOME/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                              && \
