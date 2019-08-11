31a04e3a5a4c958676281b5da35e33fa32d95ae7
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-11 10:44:53 +0530

    Update nexusoss2.sh

--- nexusoss2.sh
+++ nexusoss2.sh
@@ -2,12 +2,13 @@ sudo yum -y update
 
 sudo yum -y install java-1.8.0-openjdk
 
-curl -J -L https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz -o $HOME/nexus.tar.gz && \
-tar xzf $HOME/nexus.tar.gz -C $HOME/                                                                && \
-rm -rf  $HOME/nexus.tar.gz                                                                          && \
-mv      $HOME/nexus-*       $HOME/.nexusoss2                                                        && \
-mv      $HOME/sonatype-work $HOME/.artifactory                                                      && \
-find    $HOME/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                              && \
-find    $HOME/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                              && \
-find    $HOME/.artifactory   -maxdepth 1 -name "*.txt" -type f -delete                              && \
-rm -rf  $HOME/.nexusoss2/bin/jsw/{license,linux-ppc*,linux-x86-32,macosx*,solaris*,windows*}
+mkdir root                                                                                               && \
+curl -J -L https://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz -o $HOME/root/nexus.tar.gz && \
+tar xzf $HOME/root/nexus.tar.gz -C $HOME/root/                                                           && \
+rm -rf  $HOME/root/nexus.tar.gz                                                                          && \
+mv      $HOME/root/nexus-*       $HOME/root/.nexusoss2                                                   && \
+mv      $HOME/root/sonatype-work $HOME/root/.artifactory                                                 && \
+find    $HOME/root/.nexusoss2     -maxdepth 1 -name "*.txt" -type f -delete                              && \
+find    $HOME/root/.nexusoss2/bin -maxdepth 1 -name "*.bat" -type f -delete                              && \
+find    $HOME/root/.artifactory   -maxdepth 1 -name "*.txt" -type f -delete                              && \
+rm -rf  $HOME/root/.nexusoss2/bin/jsw/{license,linux-ppc*,linux-x86-32,macosx*,solaris*,windows*}  
